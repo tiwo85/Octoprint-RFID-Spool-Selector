95c946034a627008a5c207eb8dc4d31204a11ceb
tiwo85 <timo.wozasek@googlemail.com>
2019-11-23 21:59:12 +0100

    -

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 
 #ifndef BUILD_NUMBER
-  #define BUILD_NUMBER "3"
+  #define BUILD_NUMBER "4"
 #endif
 #ifndef VERSION
-  #define VERSION "v0.6.191123_3 - 2019-11-23 21:46:34.503000"
+  #define VERSION "v0.6.191123_4 - 2019-11-23 22:14:13.327000"
 #endif
 #ifndef VERSION_SHORT
-  #define VERSION_SHORT "v0.6.3"
+  #define VERSION_SHORT "v0.6.4"
 #endif
